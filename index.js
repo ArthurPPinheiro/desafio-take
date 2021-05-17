ac92fe4cea9ddcc223f685647e7a240e6adcdf12
Arthur Pinheiro <arthurppinheiro@hotmail.com>
2021-05-17 20:49:43 -0300

    users teste

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello') );
+var users = [
+    {id: 1, name: "John", email: "john@mail.com", password: "john123"},
+    {id: 2, name: "Sarah", email: "sarah@mail.com", password: "sarah123"}
+];
+
+app.get('/', function(){
+    res.json(users);
+});
 
 app.listen(process.env.PORT || 3000);
