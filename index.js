b37e5bc5262633497b4c0941d6f619f8541f149e
Arthur Pinheiro <arthurppinheiro@hotmail.com>
2021-05-17 21:31:31 -0300

    teste

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 
 const hostname = 'https://api.github.com/';
 const path = 'repos/takenet/ServiceStack.Text';
-
-request(`${hostname}${path}`, (error, response, body) => {
+request(`https://api.github.com/repos/takenet/ServiceStack.Text`, (error, response, body) => {
     var github = body;
+    var github_response = response;
 })
 
 app.get('/api/github', function(req, res){
-    res.json(github);
+    res.json(github, github_response);
 });
 
 app.listen(process.env.PORT || 3000);
