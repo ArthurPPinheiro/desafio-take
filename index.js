1d587951e4c6c5836e308498db5b6acd1a66c9e1
Arthur Pinheiro <arthurppinheiro@hotmail.com>
2021-05-17 22:44:17 -0300

    teste

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const hostname = 'https://api.github.com/';
 const path = 'repos/takenet/ServiceStack.Text';
 let github = '';
 request(`https://api.github.com/orgs/takenet/repos?sort=created&direction=asc&per_page=5&page=1`, (error, response, body) => {
-    console.log(response);
+    console.log(body);
+    console.log(response)
 })
-console.log(body);
 app.get('/api/github', function(req, res){
     res.json('');
 });
