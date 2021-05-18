6eb132fe9b83e0cdf93531a87826b742285ed43a
Arthur Pinheiro <arthurppinheiro@hotmail.com>
2021-05-17 22:33:13 -0300

    fix

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const request = require('request');
 const hostname = 'https://api.github.com/';
 const path = 'repos/takenet/ServiceStack.Text';
 let github = '';
-request(`https://api.github.com/repos/takenet/ServiceStack.Text`, (error, response, body) => {
+request(`https://api.github.com/orgs/takenet/repos?sort=created&direction=asc&per_page=5&page=1`, (error, response, body) => {
+    console.log(body);
     github = body;
 })
-
+console.log(body);
 app.get('/api/github', function(req, res){
-    res.json(github);
+    res.json('');
 });
 
 app.listen(process.env.PORT || 3000);
